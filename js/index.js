0511a6bbb6b12b0704c3e0076e6408023ad81889
klovaaxel <klovakarlsson@gmail.com>
2022-03-13 11:50:38 +0100

    Basic working js for populating index.html

---
+++ js/index.js
@@ -0,0 +1,59 @@
+//Define common variables
+courseList = document.querySelectorAll('[type=course]');
+mdUrl = url = 'https://raw.githubusercontent.com/klovaaxel/schedule/main/md/';
+scheduleUrl = 'schedule.html'
+//Get all groups
+groupList = document.querySelectorAll('[class*=group]');
+console.log(groupList);
+//Get params from url
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+group = urlParams.get("group");
+
+//If group specified, hide all others
+if (group != null) {
+    groupList.forEach(groupItem => {
+        if (groupItem.id != group) {
+            groupItem.classList.add("hide");
+        }
+    });    
+}
+
+//Fill week number in title
+document.getElementById("week-number").innerHTML = getWeekNumber();
+
+//Fill Courses in each group section
+courseList.forEach(course => {
+    fetchMarkdown(url + course.id + '.md')
+        .then(response =>{
+            mdToHtml(response)
+                .then(response => {
+                    coursePlan = document.createElement("div");
+                    coursePlan.innerHTML = response;
+                    currentWeekPlan = document.createElement("div");
+                    currentWeekPlan.innerHTML = coursePlan.querySelector("[id=vecka10]").nextElementSibling.innerHTML;
+
+                    course.appendChild(currentWeekPlan);
+                    course.href = scheduleUrl + "?course=" + course.id;
+                })
+        });
+});
+
+//Fetch markdown from url
+async function fetchMarkdown(url){
+    return await fetch(url) 
+        .then(response => response.text())
+        .then(result => {
+            return result;
+        });
+}
+
+//convert markdown to HTML
+async function mdToHtml(md){
+    var converter = new showdown.Converter();
+    text = md;
+    html = converter.makeHtml(text);
+    
+    return html
+}
+
