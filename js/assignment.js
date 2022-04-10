e8f7f9394ec6c8074e98f399c9ff185bbdad6246
klovaaxel <klovakarlsson@gmail.com>
2022-04-11 00:41:10 +0200

    Basic js to get assignments

---
+++ js/assignment.js
@@ -0,0 +1,72 @@
+//Get and store groups/courses
+async function getHtml(url) {
+    //Fetch html from index.html to grab group names and courses
+    await fetch(url)
+        .then(response => response.text())
+        .then(result => {
+            htmlString = result;
+        })
+
+    //Put html string into dom element
+    html = document.createElement('html');
+    html.innerHTML = htmlString;
+    html = html.lastChild;
+
+    return html
+}
+
+//Get and set courses for each group
+async function getCourses(group) {
+    courses = group.querySelectorAll('[type=course]');
+    return courses
+}
+
+//Get groupHtml in html from group name (id)
+async function getGroupHtml(html){
+    groups = html.querySelectorAll('.group');
+    return groups
+}
+
+//Get assignments from index.html
+//Get html from index.html
+getHtml('index.html')
+    .then(html => {
+        //Get groups from index.html
+        getGroupHtml(html).then(groups => {
+            for(const group of groups){
+
+                console.log(group);
+                
+                //Get courses from each group
+                getCourses(group)
+                    .then(courses => {
+                        for(const course of courses){
+
+                            console.log(course);
+
+                            //Get markdown for each course
+                            fetchMarkdown(url + course.id + '.md')
+                                .then(md => {
+                                    //Convert markdown to html
+                                    mdToHtml(md)
+                                        .then(htmlString => {
+
+                                            //Make the html string a html object and remove html parent
+                                            html = document.createElement('html');
+                                            html.innerHTML = htmlString;
+                                            html = html.lastChild;
+                                            
+                                            //Get assignments from each course
+                                            getAssignments(html)
+                                                .then(assignments => {
+                                                    for(const assignment of assignments){
+                                                        console.log(assignment);
+                                                    }
+                                                });
+                                        })
+                                })
+                        }
+                    })
+            }
+        });
+    });
