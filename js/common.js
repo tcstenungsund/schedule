57e7cfad3c67bda706aefcc2bd05371bb230b192
Kenneth Frantzen <5168530+seetee@users.noreply.github.com>
2022-10-06 11:07:29 +0200

    Added Projekt to list of assignments

--- js/common.js
+++ js/common.js
@@ -146,6 +146,7 @@ async function getAssignments(html){
         contains(., 'PROV')or \
         contains(., 'Laboration')or \
         contains(., 'Lab')or \
+        contains(., 'Projekt')or \
         contains(., 'Djupdykning')"
 
     const highlightAssignmentKeyWords = [
