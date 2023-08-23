4583e22c277e4330facad4fd390f1cca02643624
klovaaxel <klovakarlsson@gmail.com>
2023-08-23 20:35:35 +0200

    Add week 34 as a school week

--- js/common.js
+++ js/common.js
@@ -14,9 +14,8 @@ fontSize = urlParams.get("fontsize");
 console.log("param" + fontSize);
 //weeks in school year common variable
 weeks = [
-  35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 01, 02,
-  03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23,
+  34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 1, 2,
+  3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
 ];
 
 //Set style from local prefrence if specified
